5fe77f32116224ead72cb6e116e79dc41c2022cb
Emma Paris <anmolpal1999@gmail.com>
2021-03-25 11:13:32 +0530

    Calc App completed

--- calculator.js
+++ calculator.js
@@ -1,19 +1,37 @@
 // jshint esversion:6
 
 const express =  require("express");
-
+const bodyParser = require("body-parser");
 const app = express(); // a function that represent express module
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req,res){
-  res.send("Hello World");
-});
-//
-// app.get("/testing", function(res,req){  // argument pos is wrong
-//   res.send("<h1>HEY CUTIE!!!</h1>");
+
+// Sending file is different from sending text
+
+// app.get("/", function(req,res){
+//   res.sendFile("index.html");
 // });
 
-app.get("/testing", function(req,res){
-  res.send("<h1>Hello about</h1>");
+// instead of sending relative path what we can do is we can
+// use a constant that called __dirname (directory)
+//__dir it gives file loc no matter where your app is deployed
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+// lets add app.post() to handle any post request that come to our home route
+// here also we are going to have a call back(req,res).
+//
+
+app.post("/", function(req,res){
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  num1+=num2;
+  res.send("The result of calculation is: "+ num1);
+  // console.log(req.body.num1);
+  // res.send("Working...!!");
 });
 
 app.listen(3000, function(){
