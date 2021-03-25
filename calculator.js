572ec8d709f3ca41fe7dfe02b9aae3731c0efbf4
Emma Paris <anmolpal1999@gmail.com>
2021-03-25 13:47:51 +0530

    bmi

--- calculator.js
+++ calculator.js
@@ -20,10 +20,18 @@ app.get("/", function(req,res){
   res.sendFile(__dirname+"/index.html");
 });
 
+app.get("/bmicalculator", function(req,res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
 // lets add app.post() to handle any post request that come to our home route
 // here also we are going to have a call back(req,res).
 //
-
+app.post("/bmicalculator",function(req,res){
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var bmi = weight/(height*height);  
+  res.send("Your BMI is "+ bmi);
+});
 app.post("/", function(req,res){
 
   var num1 = Number(req.body.num1);
